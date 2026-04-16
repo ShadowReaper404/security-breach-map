cafe8b51332df1485e81e0d14bdc77f7889b5c3b
Thevindu Sithujaya <thevindusithujaya@gmail.com>
2026-04-16 22:18:52 +0530

    Add countryCoords data for mapping geographical locations

---
+++ app/components/countries.ts
@@ -0,0 +1,14 @@
+export const countryCoords: Record<string, [number, number]> = {
+  "US": [-100.0, 40.0],
+  "FR": [2.21, 46.22],
+  "LK": [80.77, 7.87],
+  "CN": [104.19, 35.86],
+  "RU": [105.31, 61.52],
+  "GB": [-3.43, 55.37],
+  "DE": [10.45, 51.16],
+  "BR": [-51.92, -14.23],
+  "AU": [133.77, -25.27],
+  "IN": [78.96, 20.59],
+  "JP": [138.25, 36.20],
+  // You can add more as you see them in your data!
+};
